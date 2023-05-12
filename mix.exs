7424fb5a7f879e069b6b4d22cf91c0b6162ac7ed
Abhinand V K <abhinandvk41@gmail.com>
2023-05-13 03:35:04 +0530

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Saturn.MixProject do
   def project do
     [
       app: :saturn,
-      version: "0.1.0",
+      version: "0.1.3",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
